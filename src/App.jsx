34ad9ecc54f579ab793941fbece196999ca9e146
Zohrab <zohu8306@gmail.com>
2025-03-20 18:17:12 +0300

    header components
    first codes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import PageContainer from './container/PageContainer'
+import Header from './components/Header'
+
+function App() {
+ 
+
+  return (
+    
+      <div>
+       <PageContainer>
+        <Header/>
+       </PageContainer>
+      </div>
+      
+    
+  )
+}
+
+export default App
